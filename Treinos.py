1d8561139a307c479c767dd795fb00ee33956383
Jonathan Marques <jonathan.marques@gobots.com.br>
2020-11-30 15:38:12 -0300

    Add função treinos

---
+++ Treinos.py
@@ -0,0 +1,13 @@
+def rotina(tipo):
+    perna = []
+    peito = []
+    costas = []
+    if tipo == 1:
+        for i in perna:
+            print(perna[i])
+    elif tipo == 2:
+        for i in peito:
+            print(peito[i])
+    elif tipo == 3:
+        for i in costas:
+            print(costas[i])
