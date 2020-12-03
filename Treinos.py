71e4027ccfc0f6ff5055f99cf9446458c155472a
Jonathan Marques <jonathan.marques@gobots.com.br>
2020-12-03 10:15:38 -0300

    Treino atualizado

--- Treinos.py
+++ Treinos.py
@@ -4,11 +4,13 @@ def rotina(objetivo):
                      'PEITO: Supino Reto, Cross Over\n'
                      'COSTAS: Pulley Frente, Remada curvada\n'
                      'PERNAS: Leg press, Flexora\n',
-                '2': '3 Séries de 15-20 repetições com 30s de descanso entre elas\n'
+
+                     '2': '3 Séries de 15-20 repetições com 30s de descanso entre elas\n'
                      'PEITO: Flexão de Braços, Crucifixo inclinado\n'
                      'COSTAS: Crucifixo invertido, Pull over\n'
                      'PERNAS: Adução de coxa, Panturrilha Sentado\n',
-                '3': '4 Séries de 4-6 repetições com 2 min de descanso entre elas\n'
+
+                     '3': '4 Séries de 4-6 repetições com 2 min de descanso entre elas\n'
                      'PEITO: Voador, Supino declinado\n'
                      'COSTAS: Remada Baixa, Pull down\n'
                      'PERNAS: Agachamento, Abdução de coxa\n'}
