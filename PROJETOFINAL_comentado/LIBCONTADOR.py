cf3e21f91c8167b0285a54b7fa6032efaf0bf65b
Vlarck <lamarck776@gmail.com>
2020-12-03 17:59:01 -0300

    Projeto_comentado

--- PROJETOFINAL_comentado/LIBCONTADOR.py
+++ PROJETOFINAL_comentado/LIBCONTADOR.py
@@ -1,28 +1,25 @@
 from tkinter import *
 
 def cadastro():
-    def contador():
-        arquivo = open("dados.txt", "a+")
-        nome=entrada1.get()
-        idade=entrada2.get()
-        peso=entrada3.get()
-        altura=entrada4.get()
-        celular=entrada5.get()
-        objetivo=entrada6.get()
-        listadados=[f'\n{nome}\n{idade}\n{peso}\n{altura}\n{celular}\n{objetivo}']
-        arquivo.writelines(listadados)
-        arquivo.seek(0)
-        linhas=arquivo.readlines()
-        numc=len(linhas)
-        ncad=numc/6
+    def salvadados():
+        arquivo = open("dados.txt", "a+")#abre arquivo dos dados
+        nome=entrada1.get()#recebe oq foi escrito em entrada1
+        idade=entrada2.get()#recebe entrada2
+        peso=entrada3.get()#recebe entrada3
+        altura=entrada4.get()#recebe entrada4
+        celular=entrada5.get()#recebe entrada5
+        objetivo=entrada6.get()#recebe entrada6
+        listadados=[f'\n{nome}\n{idade}\n{peso}\n{altura}\n{celular}\n{objetivo}']#coloca oq foi escrito na lista
+        arquivo.writelines(listadados)#escreve a lista no arquivo de dados(cada elemento, uma linha)
         arquivo.close()
-        cadastro.close()
     arquivo = open("dados.txt", "a+")
     arquivo.seek(0)
-    ncad=(len(arquivo.readlines())/6)+1
+    ncad=(len(arquivo.readlines())/6)+1#Numero do cadastro
     janela=Tk()
     janela.title('Cadastro')
-    Button(janela, text="Cadastrar",width=22, bg='white', fg='black',command=contador, font='None 14 bold').grid(row=8, column=0,columnspan=3, sticky=W)
+    #botão cadastrar chama função salvadados
+    Button(janela, text="Cadastrar",width=22, bg='white', fg='black',command=salvadados, font='None 14 bold').grid(row=8, column=0,columnspan=3, sticky=W)
+    #cria entradas e joga dentro da janela
     entrada1=Entry(janela, width=25, bg='white')
     entrada1.grid(row=1, column=1, sticky=W)
     entrada2=Entry(janela, width=25, bg='white')
@@ -71,10 +68,10 @@ def consulta():
                 resultado = dict_objetivo['3']
             L=Label(janela, text=resultado)
             L.grid(row=13, column=0,columnspan=4, sticky=W)
-        arquivo = open("dados.txt", "a+")
+        arquivo = open("dados.txt", "a+")#abre arquivo dos dados
         numc=(int(entrada4.get())*6)
         arquivo.seek(0)
-        listadados=arquivo.readlines()
+        listadados=arquivo.readlines()#ler arquivo de dados e colocar em lista,após o cadastro
         obj=int(listadados[numc-1])
         celular=listadados[numc-2]
         altura=float(listadados[numc-3])
