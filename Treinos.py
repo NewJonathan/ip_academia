b41cc036cbaa142776acc823f60d2a76d0dc945d
Jonathan Marques <jonathan.marques@gobots.com.br>
2020-12-03 10:14:31 -0300

    Treino atualizado

--- Treinos.py
+++ Treinos.py
@@ -1,8 +1,6 @@
-tipo = 3
 
-
-def rotina(tipo):
-    objetivo = {'1': '3 Séries de 8-12 repetições com 1 min de descanso entre elas\n '
+def rotina(objetivo):
+    dict_objetivo = {'1': '3 Séries de 8-12 repetições com 1 min de descanso entre elas\n '
                      'PEITO: Supino Reto, Cross Over\n'
                      'COSTAS: Pulley Frente, Remada curvada\n'
                      'PERNAS: Leg press, Flexora\n',
@@ -14,12 +12,12 @@ def rotina(tipo):
                      'PEITO: Voador, Supino declinado\n'
                      'COSTAS: Remada Baixa, Pull down\n'
                      'PERNAS: Agachamento, Abdução de coxa\n'}
-    if tipo == 1:
-        resultado = objetivo['1']
-    elif tipo == 2:
-        resultado = objetivo['2']
-    elif tipo == 3:
-        resultado = objetivo['3']
+    if objetivo == 1:
+        resultado = dict_objetivo['1']
+    elif objetivo == 2:
+        resultado = dict_objetivo['2']
+    elif objetivo == 3:
+        resultado = dict_objetivo['3']
     return resultado
 
-print(rotina(tipo))
+
